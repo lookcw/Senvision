853919e9bda896d427d6557b2d0f609de252af29
Christopher Look <LookWChristopher@gmail.com>
2016-11-12 16:59:58 -0500

    added url fetcher

--- NewsFetchers/query.py
+++ NewsFetchers/query.py
@@ -1,11 +1,20 @@
 from eventregistry import *
 er = EventRegistry()
 q = QueryArticles()
-# set the date limit of interest
-q.setDateLimit(datetime.date(2014, 4, 16), datetime.date(2014, 4, 28))
-# find articles mentioning the company Apple
-q.addConcept(er.getConceptUri("Apple"))
-# return the list of top 30 articles, including the concepts, categories and article image
-q.addRequestedResult(RequestArticlesInfo(page = 1, count = 30, 
-    returnInfo = ReturnInfo(articleInfo = ArticleInfoFlags(concepts = True, categories = True, image = True))))
-res = er.execQuery(q)
+
+def getArticlesUrls(queryterm,date):
+
+	#set the date limit of interest
+	q.setDateLimit(datetime.date(2014, 4, 16), datetime.date(2014, 4, 28))
+	# find articles mentioning the company Apple		
+	q.addConcept(er.getConceptUri("Apple"))
+	# return the list of top 30 articles, including the concepts, categories and article image
+	q.addRequestedResult(RequestArticlesUrlList(page=1,count=30))
+	writefile=open(queryterm+"_"+date+"_Urls.txt",'w')
+	
+	results=str(er.execQuery(q))
+	writefile.write(results)
+	writefile.close()
+
+
+getArticlesUrls("Apple","2014-4-16")
