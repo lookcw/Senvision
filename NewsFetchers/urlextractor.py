ddd52a203bb78dfb466eb3886957ea40c5063bb4
Christopher Look <LookWChristopher@gmail.com>
2016-11-13 03:06:39 -0500

    removed weird html file

--- NewsFetchers/urlextractor.py
+++
@@ -1,7 +0,0 @@
-import json
-from pprint import pprint
-
-with open('query2.json') as data_file:    
-    data = json.load(data_file)
-
-pprint(data)
--- src/EventRegistryUrlPulling.java
+++ src/EventRegistryUrlPulling.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import org.apache.commons.validator.routines.UrlValidator;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -14,6 +15,10 @@ public class EventRegistryUrlPulling {
 	public static String getBodyParagraph(String url) {
 		Document doc;
 		Elements paragraphs;
+		UrlValidator urlValidator = new UrlValidator();
+		System.out.println(url);
+		System.out.println(urlValidator.isValid(url));
+		if(urlValidator.isValid(url)){
 		try {
 			doc = Jsoup.connect(url).get();
 			//System.out.println(doc.select("p[class=article-published]").toString());
@@ -25,6 +30,9 @@ public class EventRegistryUrlPulling {
 		}
 		//System.out.println(doc.title());
 		return doc.body().text();
+		}
+		else
+			return "";
 //		return paragraphs.toString().replaceAll("<p>", " ");
 	}
 	public static void WriteArticles(String inputFolder,String outputFolder) throws IOException{
