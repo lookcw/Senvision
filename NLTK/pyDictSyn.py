e9bd4afd2d3d96159440d9b50a6faa55b523af6a
ChrisLook <LookWChristopher@gmail.com>
2016-12-29 13:08:10 -0500

    made pyDict right

--- NLTK/pyDictSyn.py
+++ NLTK/pyDictSyn.py
@@ -1,6 +1,38 @@
 from PyDictionary import PyDictionary
 import BeautifulSoup
+import os
+import sys
+import csv
+
+vocab_folder="../CorrelationAnalysis/Vocab/"
+all_synonyms_folder="../CorrelationAnalysis/synonyms/"
 dictionary=PyDictionary()
-dict= (dictionary.synonym("Good"))
-for x in dict:
-	print dict[x]
+
+def writeSyns(word):
+	towrite=[]
+	syns=""
+	dict= (dictionary.synonym(word,"lxml"))
+	if dict:
+		for x in dict:
+			towrite.append(x.rstrip("\n"))
+			for syn in dict[x]:
+				syns+= str(syn) + "_"
+		towrite.append(syns)
+		return towrite
+	return towrite
+
+
+
+for subdir, dirs, files in os.walk(vocab_folder):
+    for file in files:
+	vocab_file=vocab_folder+file
+	all_synonyms_file=all_synonyms_folder+	 file[0:file.find("_")+1]+"_synonyms.csv"
+	output=open(all_synonyms_file,'w')
+	csvwriter=csv.writer(output,delimiter=",")
+	vocab=open(vocab_file,"r")
+	with vocab as f:
+	    for line in f:
+	    	print line,
+	    	print writeSyns(line)
+	        csvwriter.writerow(writeSyns(line))
+
