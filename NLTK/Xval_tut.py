1d9f3618ba1040892f03d03c6ef136ce61f01dd7
ChrisLook <LookWChristopher@gmail.com>
2016-12-30 14:26:39 -0500

    added new Xval_tut.py

--- NLTK/Xval_tut.py
+++ NLTK/Xval_tut.py
@@ -9,7 +9,7 @@ import csv
 import sys
 
 
-descriptor_dir = '../CorrelationAnalysis/newsdescriptors'	
+descriptor_dir = '../CorrelationAnalysis/newsdescriptors/'	
 results_file=open('../Results/sklearn_results.csv','a')
 result_writer=csv.writer(results_file,delimiter=',')
 n = 1
@@ -23,17 +23,18 @@ if identifier == "default identifier":
 	print "error: put in identifier with -iden arg"
 	sys.exit(0)
 now = datetime.datetime.now()
-result_writer.writerow([now,identifier])
+if identifier!=0:
+	result_writer.writerow([now,identifier])
 for subdir, dirs, files in os.walk(descriptor_dir):
     for file in files:
 	des_file=open(os.path.join(subdir, file),'r')
 	reader=csv.reader(des_file,delimiter='\t')
 	array=np.array(list(reader))
-	print array
+	print array.shape
 	comp_name=array[1][1]
 	array=array[1:]#take out company names names
-	data=array[:][:,2:-1] #extract the training data without target
-	target=array[:][:,-1] #extract target
+	data=array[:][:,2:-2] #extract the training data without target
+	target=array[:][:,-2] #extract target
 	#(X_train,X_test,y_train,y_test)=train_test_split(data,target,test_size=0.2,random_state=0)
 	clf=svm.SVC(kernel='rbf',C=100,decision_function_shape=None) #create svm
 	scores = cross_val_score(clf,data,target,cv=5,scoring='accuracy')#cv its number of folds 
