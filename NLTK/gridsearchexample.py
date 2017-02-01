f41e37970f020a4a702d43dee54891f7321d2ba0
ChrisLook <LookWChristopher@gmail.com>
2017-02-01 18:59:01 -0500

    added more descriptors

---
+++ NLTK/gridsearchexample.py
@@ -0,0 +1,19 @@
+from sklearn import svm, datasets
+from sklearn.model_selection import GridSearchCV
+iris = datasets.load_iris()
+parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10]}
+svr = svm.SVC()
+clf = GridSearchCV(svr, parameters)
+clf.fit(iris.data, iris.target)
+                           
+
+
+
+
+
+
+
+
+
+sorted(clf.cv_results_.keys())
+ 
