13c7542cf2151d29f63e88d24f9f7438dfa45cf9
ChrisLook <LookWChristopher@gmail.com>
2017-02-10 23:45:17 -0500

    added ticker predictions

---
+++ Results/replace_with_tickers.sh
@@ -0,0 +1,8 @@
+sed -i -- 's/Intel/INTC/g' $1
+sed -i -- 's/JPMorganChase/JPM/g' $1
+sed -i -- 's/Google/GOOG/g' $1
+sed -i -- 's/Boeing/BA/g' $1
+sed -i -- 's/Merck/MRK/g' $1
+sed -i -- 's/Apple/AAPL/g' $1
+sed -i -- 's/Procter&Gamble/PG/g' $1
+sed -i -- 's/Walmart/WMT/g' $1
