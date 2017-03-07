3699cf8798d788f0806a43c4103e11fff931c181
ChrisLook <LookWChristopher@gmail.com>
2017-03-07 00:21:20 -0500

    added a lot of data

--- NLTK/regNER.py
+++ NLTK/regNER.py
@@ -1,4 +1,6 @@
-#creates descriptors based on NER
+#creates descriptors based on NER of either news or tweets. These 
+#descriptors are in the form of a csv where the first column 
+#is the date and the second is a dictionary of whether the NER exists or not.
 import nltk 
 # from nltk.corpus import state_union
 # sample = state_union.raw("2006-GWBush.txt")
@@ -34,12 +36,12 @@ def extract_entity_names(t):
 
 output=''
 if data_type=="tweet":
-	data_dir='../AllTweets/filteredTweets'
-	common_words_file="News_Common_NER/"
-	output="news_NER_descriptors"
+	data_dir='../AllTweets/filteredTweets' #the tweet data
+	common_words_file="Tweet_Common_NER" #the tweet common words from the above 
+	output="news_NER_descriptors" #the folder the desciptors will be put into.
 if data_type=="news":
 	data_dir='../News/ArticlesData'
-	common_words_file="Tweet_Common_NER"
+	common_words_file="News_Common_NER"
 	output=="twitter_NER_descriptors"
 
 #find x most common NER
@@ -78,14 +80,28 @@ def find_features_comp(subdir):
 	print subdir
 	comp=subdir.split("/")[-1]
 	files = os.walk(subdir).next()[2]
+
 	#######opening files per comp
 	stock_file=open("../stock_data/tweet_date_data/"+comp+"_cleaned.tsv",'r')
 	stock_reader=csv.reader(stock_file,delimiter='\t')
 	common_words=open(common_words_file+"/"+comp+"_top_"+str(num_common_words)+"_words.tsv",'r')
 	word_reader=csv.reader(common_words,delimiter='\t')
-	descriptor_file=open(output+comp+"_descriptor.tsv",'w')
+	dates_done=[]
+
+	#If com is yes, then write all new descriptors, otherwise just append.
+	if(com=="y"):
+		descriptor_file=open(output+"/"+comp+"_descriptor.tsv",'w')
+	else:
+		descriptor_file=open(output+"/"+comp+"_descriptor.tsv",'a')
+		descriptor_file_read=open(output+"/"+comp+"_descriptor.tsv",'r')
+		descriptor_reader=csv.reader(descriptor_file_read,delimiter='\t')
+		descriptor_lines=list(descriptor_reader)
+		for line in descriptor_lines:
+			dates_done.append(line[0])
 	descriptor_writer=csv.writer(descriptor_file,delimiter='\t')
-	word_features=next(word_reader)
+
+
+	word_features=next(word_reader) #the common words 
 	stock_lines=list(stock_reader)
 	stock_dict={}
 	descriptors=[]
@@ -94,34 +110,39 @@ def find_features_comp(subdir):
 	for line in stock_lines:
 		stock_dict[line[1]]=line[-2]
 
+
 	#create descriptors and add value to end
 	if (len(files) > 0): 
 		for file in files:
 			print file
 			entity_names=[]
 			filec = file.replace('+', '\+')
-			filename=subdir + "/" + file
-			with codecs.open(filename, 'r',encoding='latin-1') as f:
-				sample = f.read()
-			sentences = nltk.sent_tokenize(sample)
-			tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
-			tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
-			chunked_sentences = nltk.ne_chunk_sents(tagged_sentences, binary=True)
-			for tree in chunked_sentences:
-			    entity_names.extend(extract_entity_names(tree))
 			if(data_type=="tweet"):
 				date = file.split('_')[1].split("Tweet")[0]
 			if(data_type=="news"):
 				date = file.split('_')[1]
-			print date
-			features = {}
-			for w in word_features:
-				k=w in entity_names
-				features[w]=k
-				#features[w] = (all_words[w])
-			if date in stock_dict:
-				descriptors.append([date,features,stock_dict[date]])
-				descriptor_writer.writerow([date,features,stock_dict[date]])
+			filename=subdir + "/" + file
+			print dates_done
+			if date not in dates_done and com!="y":
+				with codecs.open(filename, 'r',encoding='latin-1') as f:
+					sample = f.read()
+
+				#start nltk analysis
+				sentences = nltk.sent_tokenize(sample)
+				tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
+				tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
+				chunked_sentences = nltk.ne_chunk_sents(tagged_sentences, binary=True)
+				for tree in chunked_sentences:
+				    entity_names.extend(extract_entity_names(tree))
+				print date
+				features = {}
+				for w in word_features:
+					k=w in entity_names
+					features[w]=k
+				#write nltk results
+				if date in stock_dict:
+					descriptors.append([date,features,stock_dict[date]])
+					descriptor_writer.writerow([date,features,stock_dict[date]])
 
 for subdir in subdirs[1:]:
 	find_features_comp(subdir)
