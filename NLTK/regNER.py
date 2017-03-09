ee5d8c3e84c27ddd44055b8a30e172627a219b50
ChrisLook <LookWChristopher@gmail.com>
2017-03-08 21:41:49 -0500

    currently running descriptor

--- NLTK/regNER.py
+++ NLTK/regNER.py
@@ -59,7 +59,7 @@ if(com=='y'):
 			for file in files:
 				filename=subdir + "/" + file
 				print filename
-				with open(filename, 'r',encoding='latin-1') as f:
+				with codecs.open(filename, 'r',encoding='latin-1') as f:
 					sample = f.read()
 				sentences = nltk.sent_tokenize(sample)
 				tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
