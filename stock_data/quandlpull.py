7357972943510758ff0bee3cca690ce914be0260
sannamadan <sanna.madan@gmail.com>
2017-05-30 15:07:46 -0400

    fixed pulling with quandl

--- stock_data/quandlpull.py
+++ stock_data/quandlpull.py
@@ -6,27 +6,35 @@ import time
 auth_tok = "sJ1-JesK6dU83RFsYBkF"
 
 #print (type(data))
-data_intel = quandl.get("WIKI/INTC", trim_start = "2016-07-01", trim_end = "2017-3-30", authtoken=auth_tok)
-data_intel.to_csv('intel.csv', header=True, index=True, sep=',')
+data_intel = quandl.get("WIKI/INTC", trim_start = "2016-07-01", trim_end = "2017-5-30", authtoken=auth_tok)
+f = open("alldata/Intel.csv", 'w')
+data_intel.to_csv('alldata/Intel.csv', header=True, index=True, sep=',')
 
-data_apple = quandl.get("WIKI/AAPL", trim_start = "2016-07-01", trim_end = "2017-3-30", authtoken=auth_tok)
-data_apple.to_csv('apple.csv', header=True, index=True, sep=",")
+data_apple = quandl.get("WIKI/AAPL", trim_start = "2016-07-01", trim_end = "2017-5-30", authtoken=auth_tok)
+f = open("alldata/Apple.csv", 'w')
+data_apple.to_csv('alldata/Apple.csv', header=True, index=True, sep=",")
 
-data_boeing = quandl.get("WIKI/BA", trim_start = "2016-07-01", trim_end = "2017-3-30", authtoken=auth_tok)
-data_boeing.to_csv('boeing.csv', header=True, index=True, sep=",")
+data_boeing = quandl.get("WIKI/BA", trim_start = "2016-07-01", trim_end = "2017-5-30", authtoken=auth_tok)
+f = open("alldata/Boeing.csv", 'w')
+data_boeing.to_csv('alldata/Boeing.csv', header=True, index=True, sep=",")
 
-data_google = quandl.get("WIKI/GOOGL", trim_start = "2016-07-01", trim_end = "2017-3-30", authtoken=auth_tok)
-data_google.to_csv('google.csv', header=True, index=True, sep=",")
+data_google = quandl.get("WIKI/GOOGL", trim_start = "2016-07-01", trim_end = "2017-5-30", authtoken=auth_tok)
+f = open("alldata/Google.csv", 'w')
+data_google.to_csv('alldata/Google.csv', header=True, index=True, sep=",")
 
-data_jpmorganchase = quandl.get("WIKI/JPM", trim_start = "2016-07-01", trim_end = "2017-3-30", authtoken=auth_tok)
-data_jpmorganchase.to_csv('jpm.csv', header=True, index=True, sep=",")
+data_jpmorganchase = quandl.get("WIKI/JPM", trim_start = "2016-07-01", trim_end = "2017-5-30", authtoken=auth_tok)
+f = open("alldata/JPMorganChase.csv", 'w')
+data_jpmorganchase.to_csv('alldata/JPMorganChase.csv', header=True, index=True, sep=",")
 
-data_merck = quandl.get("WIKI/MRK", trim_start = "2016-07-01", trim_end = "2017-3-30", authtoken=auth_tok)
-data_merck.to_csv('merck.csv', header=True, index=True, sep=",")
+data_merck = quandl.get("WIKI/MRK", trim_start = "2016-07-01", trim_end = "2017-5-30", authtoken=auth_tok)
+f = open("alldata/Merck.csv", 'w')
+data_merck.to_csv('alldata/Merck.csv', header=True, index=True, sep=",")
 
-data_png = quandl.get("WIKI/PG", trim_start = "2016-07-01", trim_end = "2017-3-30", authtoken=auth_tok)
-data_png.to_csv('png.csv', header=True, index=True, sep=",")
+data_png = quandl.get("WIKI/PG", trim_start = "2016-07-01", trim_end = "2017-5-30", authtoken=auth_tok)
+f = open("alldata/Procter&Gamble.csv", 'w')
+data_png.to_csv('alldata/Procter&Gamble.csv', header=True, index=True, sep=",")
 
-data_walmart = quandl.get("WIKI/WMT", trim_start = "2016-07-01", trim_end = "2017-3-30", authtoken=auth_tok)
-data_walmart.to_csv('walmart.csv', header=True, index=True, sep=",")
+data_walmart = quandl.get("WIKI/WMT", trim_start = "2016-07-01", trim_end = "2017-5-30", authtoken=auth_tok)
+f = open("alldata/Walmart.csv", 'w')
+data_walmart.to_csv('alldata/Walmart.csv', header=True, index=True, sep=",")
 
--- stock_data/stocktable.py
+++ stock_data/stocktable.py
@@ -1,17 +1,22 @@
 import csv
+import os
+import codecs
 
 #noticed: closing price != opening price next day
-companies = ['apple', 'boeing', 'google', 'intel', 'merck', 'jpm', 'walmart', 'pandg']
+#companies = ['apple', 'boeing', 'google', 'intel', 'merck', 'jpm', 'walmart', 'pandg']
 
-for company in companies:
+for filename in os.listdir("./alldata"):
 	arr = []
-	f = open('./alldata/' + str(company)+ ".csv", "r")
-	reader = csv.reader(f, delimiter=",")
-	for row in reader:
-		arr.append(row)
+	f = open('./alldata/' + str(filename), "r")
+	try:
+		reader = csv.reader(f, delimiter=",")
+		for row in reader:
+			arr.append(row)
+	except csv.Error:
+		continue
 
 	#print(arr[0])
-	#col 1 is open and col 4 is close
+	#col 9 is open and col 12 is close
 	#separate headings of array from its data
 
 	headings = arr[0]
@@ -20,24 +25,22 @@ for company in companies:
 	arr.remove(arr[0])
 
 	#strip down to first few dates
-	newarr = arr[0:10]
-	for row in newarr:
-		if float(row[1]) > float(row[4]):
-			row.append('-')
-		elif float(row[1]) < float(row[4]):
+	for row in arr:
+		if float(row[11]) > float(row[8]):
 			row.append('+')
-		elif float(row[1]) == float(row[4]):
-			row.append('no change')
-
-		pcent_change = ( (float(row[4]) - float(row[1])) / float(row[1]) ) * 100
+		elif float(row[11]) < float(row[8]):
+			row.append('-')
+		elif float(row[11]) == float(row[8]):
+			row.append('-')
+		pcent_change = ( (float(row[11]) - float(row[8])) / float(row[8]) ) * 100
 		row.append(pcent_change)
 
-	outf = open('./cleaned_data/' + str(company) + ".csv", "w")
+	outf = open('./cleaned_data/' + str(filename).replace(".csv","") + "_cleaned.csv", "w")
 
 	for x in headings:
 		outf.write(str(x) + "\t")
 	outf.write("\n")
-	for row in newarr:
+	for row in arr:
 		for x in row:
 			outf.write(str(x) + "\t")
 		outf.write("\n")
